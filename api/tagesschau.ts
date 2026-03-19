148fbe71265e292b3e5c0b0321c47fd2ea96bbe7
lorisobi <lorisobi@outlook.com>
2026-03-19 15:36:07 +0100

    24 commit

--- api/tagesschau.ts
+++ api/tagesschau.ts
@@ -156,7 +156,7 @@ export default async function handler(req: any, res: any) {
     }
 
     return res.status(200).json({ 
-        message: 'VERSION 2.1 - News Summarizer Running!',
+        message: 'VERSION 2.2 - News Summarizer Running!',
         results, 
         debugLogs 
     });
